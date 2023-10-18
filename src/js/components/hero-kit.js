bdb61d2f8494ced9b0e0a5a116b33acfbf18e33c
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-10-18 12:02:15 +0500

    fixes done

--- src/js/components/hero-kit.js
+++ src/js/components/hero-kit.js
@@ -4,18 +4,31 @@ import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 const img = document.querySelector(".hero__image-kit");
 const btn = document.querySelector(".hero__button");
 
+const mediaQuery1024 = window.matchMedia("(max-width: 1024px)");
 const mediaQuery768 = window.matchMedia("(max-width: 768px)");
 const mediaQuery480 = window.matchMedia("(max-width: 480px)");
 
 gsap.registerPlugin(MotionPathPlugin);
 if (!mediaQuery480.matches) {
-	gsap.set(img, { xPercent: -50, yPercent: -50, transformOrigin: "50% 50%" });
+	gsap.set(img, { xPercent: -200, yPercent: -25, transformOrigin: "0 50%" });
 	if (mediaQuery768.matches) {
+		gsap.set(img, { xPercent: -200, yPercent: 0, transformOrigin: "0 50%" });
 		gsap.to(img, {
 			x: 0,
-			duration: 5,
+			duration: 7,
 			motionPath: {
-				path: "#path-tablet",
+				path: "M2320.5 285C2320.5 285 1594 18.9998 1064 1.49981C563.473 -15.027 0.999955 378.5 0.999955 378.5",
+				autoRotate: 150,
+			},
+			repeat: -1,
+		});
+	} else if (mediaQuery1024.matches) {
+		gsap.set(img, { xPercent: -200, yPercent: 0, transformOrigin: "0 50%" });
+		gsap.to(img, {
+			x: 0,
+			duration: 7,
+			motionPath: {
+				path: "M2320.5 285C2320.5 285 1594 18.9998 1064 1.49981C563.473 -15.027 0.999955 378.5 0.999955 378.5",
 				autoRotate: 150,
 			},
 			repeat: -1,
@@ -23,9 +36,9 @@ if (!mediaQuery480.matches) {
 	} else {
 		gsap.to(img, {
 			x: 0,
-			duration: 6,
+			duration: 7,
 			motionPath: {
-				path: "#path",
+				path: "M2320.5 285C2320.5 285 1594 18.9998 1064 1.49981C563.473 -15.027 0.999955 378.5 0.999955 378.5",
 				autoRotate: 150,
 			},
 			repeat: -1,
